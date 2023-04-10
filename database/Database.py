eed0d8756770ba15849b014aea36f55db90b9467
Thommyebere <75401236+Thommyebere@users.noreply.github.com>
2023-04-10 09:00:07 -0400

    Added ability to create an account with Mongodb

--- database/Database.py
+++ database/Database.py
@@ -1,60 +1,34 @@
 import mysql.connector
 from mysql.connector import Error
 
+from pymongo import MongoClient
 
-class Database:
 
-    def __init__(self, host, database, port, user, password):
-        self.host = host
-        self.database = database
-        self.port = port
-        self.user = user
-        self.password = password
+class Database(object):
 
-    @classmethod
-    def establishconnection(cls):
-        try:
-            connection = mysql.connector.connect(host="localhost",
-                                                 database="BankApp",
-                                                 port="3306",
-                                                 user="root",
-                                                 password="Prick123$")
-            if connection.is_connected():
-                db_info = connection.get_server_info()
-                print("Connection to mysql server", db_info)
-                cursor = connection.cursor()
-                cursor.execute("Select database ();")
-                record = cursor.fetchone()
-                print("You are connected to the database", record)
-            return connection
+    def createconnection(self):
+        client = MongoClient()
+        return client
 
-        except Error as e:
-            print("Error while connecting to mysql database", e)
-        # finally:
-        #     if connection.is_connected():
-        #         cursor.close()
-        #         connection.close()
-        #         print("my connection is closed ")
-        return e
+    def createdatabase(self):
+        db = connect.createconnection().BankAccount
+        return db
+
+    def createcollection(self):
+        account_data = connect.createdatabase().account_data
+        return account_data
 
     @staticmethod
-    def createtable():
+    def insertData(data):
+        datainserted = connect.createcollection().insert_one(data)
+        return datainserted
 
-        CREATE_TABLE = """CREATE TABLE bankvault(
-        id int(11) NOT NULL AUTOINCREMENT,
-        ACCOUNT_NO INT(11) NOT NULL, 
-        FIRST_NAME VARCHAR(255) NOT NULL,
-        LAST_NAME VARCHAR(255) NOT NULL,
-        ADDRESS VARCHAR(255) NOT NULL,
-        EMAIL VARCHAR (255) NOT NULL,
-        MOBILE_NUMBER INT(20) NOT NULL,
-        PASSWORD VARCHAR (255) NOT NULL, 
-        PRIMARY KEY(ACCOUNT_NO))"""
-
-        cursor = connector.cursor()
-        result = cursor.execute(CREATE_TABLE)
-        print("Table Created")
+    @staticmethod
+    def getalldata():
+        for doc in connect.createcollection().find():
+            print(doc)
 
 
-connector = Database.establishconnection()
-Database.createtable()
+connect = Database()
+
+connect.getalldata()
--- users/Users.py
+++ users/Users.py
@@ -1,5 +1,8 @@
 from random import randint
 import maskpass
+from database import Database
+
+newData = Database.Database()
 
 
 class Users:
@@ -22,6 +25,7 @@ class Users:
         email = input("Kindly enter your email address ")
         mobile_number = input("Kindly enter your mobile number ")
         password = input("Enter your password ")
+        accountnumber = Users.generate_account_number()
         print(f"""These are the details you entered:
             first Name: {firstname}
             last Name: {lastname}
@@ -29,10 +33,17 @@ class Users:
             email:{email}
             mobile Number:{mobile_number}
             password:{password}
+            Account_number:{accountnumber}
             """)
         user_credentials = input("Press Yes if you confirm that they are correct or press No to reenter")
-        if user_credentials == "Yes":
-            return "Your account has been successfully created"
+        if user_credentials == "Yes" or "yes":
+            newuser = Users.converdatatostring(firstname, lastname, address, email, mobile_number, password,
+                                               accountnumber,
+                                               Users.deposit)
+            if newData.insertData(newuser):
+                return "Success"
+            else:
+                return "An Error occurred"
         else:
             return cls.create_account()
 
@@ -40,6 +51,20 @@ class Users:
     def generate_account_number():
         new_num = randint(10000, 99999)
         return new_num
+
+    @staticmethod
+    def converdatatostring(first_name, last_name, address, email, mobile_number, password, account_number, deposit):
+        userdata = {
+            "first_Name": first_name,
+            "last_Name": last_name,
+            "Address": address,
+            "email": email,
+            "mobile_Number": mobile_number,
+            "password": password,
+            "Account_Number": account_number,
+            "deposit": deposit
+        }
+        return userdata
 
 
 user = Users.create_account()
